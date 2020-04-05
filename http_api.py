abd457fa7ee0d75388c8e6858e1020651bbfd696
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-04-05 14:10:57 +0200

    2to3 run on example

--- http_api.py
+++ http_api.py
@@ -4,7 +4,7 @@ __version__ = '0.1.0'
 try:
     from urllib.parse import urlparse
 except ImportError:
-    from urlparse import urlparse
+    from urllib.parse import urlparse
 
 from socketserver import ThreadingTCPServer
 from http.server import SimpleHTTPRequestHandler
@@ -23,15 +23,15 @@ class LinkedInWrapper(object):
     """ Simple namespacing """
     API_KEY = environ.get('LINKEDIN_API_KEY')
     API_SECRET = environ.get('LINKEDIN_API_SECRET')
-    RETURN_URL = 'http://localhost:{0}/code'.format(globals()['PORT'])
-    authentication = LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL, PERMISSIONS.enums.values())
+    RETURN_URL = f'http://localhost:{PORT}/code'
+    authentication = LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL, list(PERMISSIONS.enums.values()))
     application = LinkedInApplication(authentication)
 
 
 liw = LinkedInWrapper()
 run_already = False
 params_to_d = lambda params: {
-    l[0]: l[1] for l in map(lambda j: j.split('='), urlparse(params).query.split('&'))
+    l[0]: l[1] for l in [j.split('=') for j in urlparse(params).query.split('&')]
 }
 
 
@@ -42,6 +42,7 @@ class CustomHandler(SimpleHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        global run_already
         parsedurl = urlparse(self.path)
         authed = liw.authentication.token is not None
 
@@ -50,17 +51,17 @@ class CustomHandler(SimpleHTTPRequestHandler):
 
             liw.authentication.authorization_code = params_to_d(self.path).get('code')
             self.wfile.write(dumps({'access_token': liw.authentication.get_access_token(),
-                                    'routes': list(filter(lambda d: not d.startswith('_'), dir(liw.application)))}).encode('utf8'))
+                                    'routes': list([d for d in dir(liw.application) if not d.startswith('_')])}).encode('utf8'))
         elif parsedurl.path == '/routes':
             self.json_headers()
 
-            self.wfile.write(dumps({'routes': list(filter(lambda d: not d.startswith('_'), dir(liw.application)))}).encode('utf8'))
+            self.wfile.write(dumps({'routes': list([d for d in dir(liw.application) if not d.startswith('_')])}).encode('utf8'))
         elif not authed:
             self.json_headers()
 
-            if not globals()['run_already']:
+            if not run_already:
                 open_new_tab(liw.authentication.authorization_url)
-            globals()['run_already'] = True
+            run_already = True
             self.wfile.write(dumps({'path': self.path, 'authed': type(liw.authentication.token) is None}).encode('utf8'))
         elif authed and len(parsedurl.path) and parsedurl.path[1:] in dir(liw.application):
             self.json_headers()
@@ -73,5 +74,5 @@ class CustomHandler(SimpleHTTPRequestHandler):
 if __name__ == '__main__':
     httpd = ThreadingTCPServer(('localhost', PORT), CustomHandler)
 
-    print('Server started on port:{}'.format(PORT))
+    print(f'Server started on port:{PORT}')
     httpd.serve_forever()
